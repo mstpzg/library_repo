d5b5590b8ce0a2fe47118c3609cca8d714c6fc2b
mstpzg <mstpzg@gmail.com>
2019-05-03 15:39:52 +0200

    "Added fields to Biblioteka class"

---
+++ src/Zadanie/Biblioteka.java
@@ -0,0 +1,19 @@
+package Zadanie;
+
+import java.util.Date;
+
+public class Biblioteka {
+   private String bookTitle,
+                  userName,
+                  userSurname;
+   private int ISBN,
+               bookLimit,
+               userId;
+   private Date borrowDate,
+                returnDate;
+   private boolean bookAvailability;
+
+
+
+
+}
