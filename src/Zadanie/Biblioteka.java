ddbc9e862b115dd18ca0e381a89f39c983e737a5
mstpzg <mstpzg@gmail.com>
2019-05-03 15:52:25 +0200

    "Added methods to Biblioteka class"

--- src/Zadanie/Biblioteka.java
+++ src/Zadanie/Biblioteka.java
@@ -3,17 +3,35 @@ package Zadanie;
 import java.util.Date;
 
 public class Biblioteka {
-   private String bookTitle,
-                  userName,
-                  userSurname;
-   private int ISBN,
-               bookLimit,
-               userId;
-   private Date borrowDate,
-                returnDate;
-   private boolean bookAvailability;
+    private String bookTitle,
+            userName,
+            userSurname;
+    private int ISBN,
+            bookLimit,
+            userId;
+    private Date borrowDate,
+            returnDate;
+    private boolean bookAvailability;
 
+    public void returnBook() {
 
+    }
 
+    public void borrowBook() {
+
+    }
+
+    public void addBook() {
+
+    }
+
+    public void removeBook() {
+
+    }
+
+    public void checkUser(){
+
+    }
 
 }
+
